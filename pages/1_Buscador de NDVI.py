4c6c52d04f6a37241b987e79c99c9dc57b9f5053
lnunez-orbely <lnunez@orbely.com>
2024-08-27 09:36:23 -0300

    Update 1_Buscador de NDVI.py

--- pages/1_Buscador de NDVI.py
+++ pages/1_Buscador de NDVI.py
@@ -6,7 +6,7 @@ import numpy as np
 import plotly.graph_objects as go
 import pandas as pd
 
-st.write(st.session_state['cred']['key_data'][0)
+st.write(st.session_state['cred']['key_data'][0))
 
 st.set_page_config('Buscador', layout="wide")
 st.logo("Orbely_Logo2.png",icon_image="Orbely_Logo2.png", link="https://orbely.com/")
